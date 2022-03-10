c41db314959aec34b93fb0d9ad89907769cb0ecf
nikky <git@nikky.moe>
2022-03-10 13:59:24 +0100

    update github-actions script

--- .github/workflows/docker_workflow.main.kts
+++ .github/workflows/docker_workflow.main.kts
@@ -2,28 +2,19 @@
 
 @file:DependsOn("it.krzeminski:github-actions-kotlin-dsl:0.10.0")
 
-import Docker_workflow_main.Util.secret
-import it.krzeminski.githubactions.actions.Action
 import it.krzeminski.githubactions.actions.actions.CacheV2
 import it.krzeminski.githubactions.actions.actions.CheckoutV2
 import it.krzeminski.githubactions.actions.docker.BuildPushActionV2
 import it.krzeminski.githubactions.actions.docker.LoginActionV1
 import it.krzeminski.githubactions.actions.docker.SetupBuildxActionV1
+import it.krzeminski.githubactions.actions.nobrayner.DiscordWebhookV1
 import it.krzeminski.githubactions.domain.RunnerType.UbuntuLatest
 import it.krzeminski.githubactions.domain.triggers.PullRequest
 import it.krzeminski.githubactions.domain.triggers.Push
 import it.krzeminski.githubactions.dsl.workflow
-import it.krzeminski.githubactions.yaml.toYaml
 import it.krzeminski.githubactions.dsl.expr
 import it.krzeminski.githubactions.yaml.writeToFile
-import java.awt.Color
 import java.nio.file.Paths
-import kotlin.io.path.writeText
-
-object Util {
-    fun secret(secret: String): String = expr("secrets.$secret")
-//    fun dollar(variable: String): String = "\$$variable"
-}
 
 val workflow = workflow(
     name = "Docker build & push",
@@ -59,8 +50,8 @@ val workflow = workflow(
         uses(
             name = "Docker Login",
             action = LoginActionV1(
-                username = secret("DOCKER_HUB_USERNAME"),
-                password = secret("DOCKER_HUB_ACCESS_TOKEN"),
+                username = expr("secrets.DOCKER_HUB_USERNAME"),
+                password = expr("secrets.DOCKER_HUB_ACCESS_TOKEN"),
             )
         )
         uses(
@@ -73,7 +64,7 @@ val workflow = workflow(
                 context = ".",
                 file = "./Dockerfile",
                 push = true,
-                tags = listOf("${secret("DOCKER_HUB_USERNAME")}/${secret("DOCKER_HUB_REPOSITORY")}:latest"),
+                tags = listOf("${expr("secrets.DOCKER_HUB_USERNAME")}/${expr("secrets.DOCKER_HUB_REPOSITORY")}:latest"),
             )
         )
         run(
@@ -91,57 +82,14 @@ val workflow = workflow(
     ) {
         uses(
             name = "Discord Workflow Status Notifier",
-            action = DiscordWebhook(
-                webhookUrl = secret("WEBHOOK_URL"),
+            action = DiscordWebhookV1(
+                githubToken = expr("github.token"),
+                discordWebhook = expr("secrets.WEBHOOK_URL"),
                 includeDetails = true,
             ),
             condition = "always()"
         )
     }
-}
-
-class DiscordWebhook(
-    val webhookUrl: String,
-    val username: String? = null,
-    val avatarUrl: String? = null,
-    val title: String? = null,
-    val description: String? = null,
-    val includeDetails: Boolean? = null,
-    val colorSuccess: Color? = null,
-    val colorFailure: Color? = null,
-    val colorCancelled: Color? = null,
-    val githubToken: String? = null,
-) : Action("nobrayner", "discord-webhook", "v1") {
-    override fun toYamlArguments(): LinkedHashMap<String, String> = linkedMapOf(
-        "github-token" to (githubToken ?: expr("github.token")),
-        "discord-webhook" to webhookUrl,
-        *listOfNotNull(
-            username?.let {
-                "username" to it
-            },
-            avatarUrl?.let {
-                "avatar-url" to it
-            },
-            title?.let {
-                "title" to it
-            },
-            description?.let {
-                "description" to it
-            },
-            includeDetails?.let {
-                "include-details" to it.toString()
-            },
-            colorSuccess?.let {
-                "color-success" to "#" + Integer.toHexString(it.rgb).substring(2)
-            },
-            colorFailure?.let {
-                "color-failure" to "#" + Integer.toHexString(it.rgb).substring(2)
-            },
-            colorCancelled?.let {
-                "color-cancelled" to "#" + Integer.toHexString(it.rgb).substring(2)
-            }
-        ).toTypedArray()
-    )
 }
 
 workflow.writeToFile(addConsistencyCheck = true)
